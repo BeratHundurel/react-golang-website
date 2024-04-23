d9d13b601e18eb1eb1b161040a5d867e12c4f969
Berat Hündürel <berat.hundurel@bilgiedu.net>
2024-04-23 20:04:28 +0300

    persist product state

--- client/src/lib/store.ts
+++ client/src/lib/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { Product } from "../models/product";
 
 export type ProductState = {
@@ -9,7 +10,14 @@ export type Actions = {
    setProducts: (products: Product[]) => void;
 };
 
-export const useProductStore = create<ProductState & Actions>()((set) => ({
-   products: [],
-   setProducts: (products: Product[]) => set({ products }),
-}));
+export const useProductStore = create<ProductState & Actions>()(
+   persist(
+      (set) => ({
+         products: [],
+         setProducts: (products: Product[]) => set({ products }),
+      }),
+      {
+         name: "productStore", // Name of the store for persistence
+      }
+   )
+);
