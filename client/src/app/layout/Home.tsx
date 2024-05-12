1a186275235a51959c734fe47ed08c11566b079b
Berat Hündürel <berat.hundurel@bilgiedu.net>
2024-05-12 03:15:02 +0300

    making products as a prop for making the component reusable

--- client/src/app/layout/Home.tsx
+++ client/src/app/layout/Home.tsx
@@ -1,14 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
 import Banner from "../../components/banner/Banner";
 import BrowseCategory from "../../components/browse/BrowseCategory";
-import HomeProducts from "../../components/product/Products";
+import Products from "../../components/product/Products";
 import Room from "../../components/room/Room";
+import { fetchProducts } from "../api/agent";
 
 export default function Home() {
+   const { data } = useQuery({ queryKey: ["products"], queryFn: fetchProducts });
+   const products = data
+      ? data
+           .slice()
+           .sort(() => Math.random() - 0.5)
+           .slice(0, 8)
+      : [];
    return (
       <>
          <Banner />
          <BrowseCategory />
-         <HomeProducts title={"Our Products"} button={true} /> // Fix: Pass the title prop as a string
+         <Products title={"Our Products"} button={true} products={products ?? []} />
          <Room />
       </>
    );
